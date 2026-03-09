ca3901b54934b3815af9584d4aa84afa5ee064d2
ghasnhhz <shahzodkarimov1001@gmail.com>
2026-03-09 06:42:17 +0500

    feat: implement apps and map urls to them

--- src/apps/products/models.py
+++ src/apps/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    seller = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='sold_products')
+    buyer = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- src/server/settings.py
+++ src/server/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users',
-    'products',
+    'apps.users',
+    'apps.products',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
